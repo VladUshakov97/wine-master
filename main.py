b71a998d28e6c1b3d629dae3be7053b76949bfcf
VladUshakov97 <vladushakov89@gmail.com>
2025-05-13 18:24:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from datetime import datetime
 from collections import defaultdict
 import pandas
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
-
 
 def year_word(age):
     if 11 <= age % 100 <= 14:
@@ -22,36 +16,43 @@ def year_word(age):
     else:
         return "лет"
 
+def main():
+    env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+    wines = pandas.read_excel('assortment.xlsx', keep_default_na=False)
+    all_wines = wines.to_dict(orient='records')
 
-wines = pandas.read_excel('assortment.xlsx', keep_default_na=False)
-all_wines = wines.to_dict(orient='records')
+    grouped = defaultdict(list)
 
-grouped = defaultdict(list)
+    for wine in all_wines:
+        category = wine['Категория']
+        drink_info = {
+            'Название': wine['Название'],
+            'Сорт': wine['Сорт'],
+            'Цена': wine['Цена'],
+            'Картинка': wine['Картинка'],
+            'Акция': wine['Акция']
+        }
+        grouped[category].append(drink_info)
 
-for wine in all_wines:
-    category = wine['Категория']
-    drink_info = {
-        'Название': wine['Название'],
-        'Сорт': wine['Сорт'],
-        'Цена': wine['Цена'],
-        'Картинка': wine['Картинка'],
-        'Акция': wine['Акция']
-    }
-    grouped[category].append(drink_info)
+    then_year = 1920
+    now_year = datetime.now().year
+    age = now_year - then_year
+    year = year_word(age)
 
-then_year = 1920
-now_year = datetime.now().year
-age = now_year - then_year
-year = year_word(age)
+    rendered_page = template.render(
+        age=age,
+        year=year,
+        wines=dict(grouped)
+    )
 
-rendered_page = template.render(
-    age=age,
-    year=year,
-    wines=dict(grouped)
-)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+if __name__ == "__main__":
+    main()
